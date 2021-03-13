729143379ab2e8f8d60848cc1553901b2e00e201
nnnlog <sorisem4106@naver.com>
2021-03-13 23:25:52 +0900

    오류 메세지 표시

--- src/component/SurveyList.vue
+++ src/component/SurveyList.vue
@@ -2,7 +2,7 @@
   <div class="body-content" data-app>
     <h3 class="body-content-header">설문 목록</h3>
     <div style="width: 100%;">
-      <div v-for="({name, url, disabled}, key) in $store.getters.surveyList" :key="key">
+      <div v-for="({name, url, disabled, error}, key) in $store.getters.surveyList" :key="key">
         <v-tooltip v-if="disabled" bottom @click="alert('이미 제출한 설문입니다.');">
           <template v-slot:activator="{ on, attrs }">
             <v-list-item v-bind="attrs" v-on="on" style="color: rgba(0, 0, 0, 0.38) !important;">
@@ -12,7 +12,7 @@
               </v-list-item-icon>
             </v-list-item>
           </template>
-          <span>이미 제출한 설문입니다.</span>
+          <span>{{ error }}</span>
         </v-tooltip>
 
         <v-list-item v-else link :to="`/survey/${url}`">
